31dc2895b7eec1f14dd516f7b627a163f8056bfc
toru.yamada <1194595t@gmail.com>
2019-11-23 05:05:46 +0000

    toppage

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+  def index
+    @post = Post.all
+  end
+  
+  def show
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+end

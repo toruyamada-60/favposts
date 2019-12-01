687f048d709fdaf123941a80f77638810169e0ed
toru.yamada <1194595t@gmail.com>
2019-12-01 09:26:02 +0000

    add count

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.order(id: :desc).page(params[:page])
+    counts(@user)
   end
 
   def new
@@ -53,12 +54,14 @@ class UsersController < ApplicationController
   def followings
     @user = User.find(params[:id])
     @followings = @user.followings.page(params[:page])
+    counts(@user)
   end
   
   
   def followers
     @user = User.find(params[:id])
     @followers = @user.followers.page(params[:page])
+    counts(@user)
   end
   
   private

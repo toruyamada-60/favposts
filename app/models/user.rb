1b43d82aa45111698b986bcffc5994dd3a7cfb33
toru.yamada <1194595t@gmail.com>
2019-12-01 08:57:25 +0000

    add dependent: :destroy for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,9 @@ class User < ApplicationRecord
   
   has_many :posts, dependent: :destroy
   
-  has_many :relationships
+  has_many :relationships, dependent: :destroy
   has_many :followings, through: :relationships, source: :follow
-  has_many :reverses_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :reverses_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy
   has_many :followers, through: :reverses_of_relationships, source: :user
 
   def follow(other_user)
